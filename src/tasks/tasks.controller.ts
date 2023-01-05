c36d7630f39d6966324a305314fd2b7c35532fb9
Svetlana K <swetlana-spb@mail.ru>
2023-01-05 12:50:40 +0400

    feat(log): logging (#5)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
   Post,
   Query,
   UseGuards,
+  Logger,
 } from '@nestjs/common';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -21,6 +22,7 @@ import { GetUser } from 'src/auth/get-user.decorator';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
   constructor(private tasksService: TasksService) {}
 
   @Get()
@@ -28,11 +30,19 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
     return this.tasksService.getTasks(filterDto, user);
   }
 
   @Get('/:id')
   getTaskById(@Param('id') id: string, @GetUser() user: User): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving a task with id ${id}`,
+    );
     return this.tasksService.getTaskById(id, user);
   }
 
@@ -41,11 +51,19 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDTO,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${
+        user.username
+      }" creating a new task with params: ${JSON.stringify(createTaskDto)}`,
+    );
     return this.tasksService.createTask(createTaskDto, user);
   }
 
   @Delete('/:id')
   deleteTaskById(@Param('id') id: string, @GetUser() user: User): void {
+    this.logger.verbose(
+      `User "${user.username}" deleting a task with id: ${id}`,
+    );
     this.tasksService.deleteTaskById(id, user);
   }
 
@@ -56,6 +74,13 @@ export class TasksController {
     @GetUser() user: User,
   ): Promise<Task> {
     const { status } = updateTaskStatusDto;
+    this.logger.verbose(
+      `User "${
+        user.username
+      }" updating a task (id = ${id}) status with following params: ${JSON.stringify(
+        updateTaskStatusDto,
+      )}`,
+    );
     return this.tasksService.updateTaskStatusById(id, status, user);
   }
 }
