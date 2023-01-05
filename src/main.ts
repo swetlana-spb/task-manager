3d0d8b986233ec5ee092993202dcbe1c10ddb85a
Svetlana <swetlana-spb@mail.ru>
2023-01-05 16:10:20 +0400

    fix(connect): port

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   const port = process.env.PORT;
-  await app.listen(3000);
+  await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
